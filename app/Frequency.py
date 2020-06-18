6c62279c5990e055886c9695a966f9c51418f33a
unknown <isayahculbertson@gmail.com>
2020-06-18 09:15:53 -0400

    added stuff

---
+++ app/Frequency.py
@@ -0,0 +1,304 @@
+from datetime import date, datetime, time, timedelta
+from os import path, getenv
+import pandas as pd
+from app.Cloud import Cloud
+from abc import abstractmethod
+from alpha_vantage.timeseries import TimeSeries
+from calendar import monthrange
+import asyncio
+
+key = alphaKey = getenv('ALPHA_KEY')
+
+class Frequency:
+    def __init__(self,symbol,start=None, end=None):
+        self.symbol = symbol.upper()
+        self.start = start
+        self.end = end
+        self.cloud = Cloud()
+
+    def properplace(self):
+        start = strToDate(self.start)
+        end = strToDate(self.end)
+        assert start < end
+
+    def fileFormat(self):
+        return f'{self}.csv'
+    
+    def __repr__(self):
+        return f'start:{self.start} end:{self.end}'
+    
+    def SaveToCloud(self):
+        self.c.storeDataonBucket(self.fileFormat())
+    
+    def loadData(self):
+        try:
+            return pd.read_csv(self.fileFormat(), index_col='date')
+        except FileNotFoundError:
+            self.cloud.downloadData(self.fileFormat(),self.fileFormat())
+            return pd.read_csv(self.fileFormat(), index_col='date')
+
+    def saveData(self,df: pd.DataFrame):
+        fileform = self.fileFormat()
+        print('saving data')
+        if path.isfile(fileform):
+            print(f"reading data from {fileform}")
+            read_df = pd.read_csv(fileform, index_col='date')
+            temp_df = read_df.combine_first(df)
+            temp_df.to_csv(fileform)
+            self.cleanData()
+        else:
+            print("creating file")
+            print(f"Creating {fileform}....")
+            df.to_csv(fileform)
+
+    def cleanData(self):
+        try:
+            x = pd.read_csv(self.fileFormat(), index_col='date')
+            duplicates = x.index.duplicated()
+            keep = duplicates == False
+            clean_data = x.loc[keep,:]
+            clean_date = clean_data.iloc[::-1]
+            clean_data.to_csv(self.fileFormat())
+        except FileExistsError:
+            print("file doesn't exist")
+
+    @abstractmethod
+    def prefix(self):
+        pass
+
+
+class IntraDay(Frequency):
+    def __init__(self, symbol, interval,end=date.today()):
+        super().__init__(symbol, end=end)
+        self.cloud = Cloud()
+        if interval not in [1,5,10,15,30,60]:
+            raise ValueError(f"valid intervals are{[1,5,10,15,30,60]}")
+        self.interval = interval
+
+    def __repr__(self):
+        return f'end:{self.end}'
+
+    def __str__(self):
+        return f'{self.symbol}_{self.interval}min_{self.end}'
+
+    def prefix(self):
+        return "_".join((str(self).split('_')[:2]))
+
+    def collectData(self) ->None:
+        week_day = date(*[int(i) for i in str(self.end).split('-')]).weekday()
+        #This means that the market is open
+        start = '9:31:00'
+        date_form_start = f'{self.end} {start}'
+
+        ts = TimeSeries(key= key, output_format='pandas')
+        data , metadata = ts.get_intraday(symbol=self.symbol,
+        interval=f'{self.interval}min', outputsize='full')
+
+        date_form_end = data.head(1).index[0].__str__()
+        print([date_form_start,date_form_end])
+
+        return_data = data.loc[self.end.__str__()]
+        self.saveData(return_data)
+        
+class MultiDay(Frequency):
+    def __init__(self, symbol, start=None, end=None):
+        super().__init__(symbol, start=start, end=end)
+        ''' 
+        proper format: {symbol}_{start_date}_{end_date}
+        '''
+        self.cloud = Cloud()
+        self.set_start_and_end()
+
+    def ValidFiles(self):
+        x = [i for i in self.cloud.get_s3_keys() if self.prefix() in i  ]
+        return x[0]
+
+    def set_start_and_end(self):
+        print('Hello')
+        try:
+            x = [i for i in self.cloud.get_s3_keys() if self.prefix() in i  ]
+            data = x[0]
+            print(data)
+            self.start , self.end = (str(x[0]).split('.')[0]).split('_')[-2:]
+            print(self.start, self.end)
+            # print(self.__repr__)
+        except IndexError:
+            print('getting new data')
+            self.collectData()
+            print(self)
+            self.start , self.end = (str(self).split('.')[0]).split('_')[-2:]
+            print(self.start, self.end)
+            print(self.__repr__)
+            start = strToDate(self.start)
+            end = strToDate(self.end)
+            assert start < end
+            self.cloud.storeDataonBucket((self.fileFormat()))
+        finally:
+            start = strToDate(self.start)
+            end = strToDate(self.end)
+            assert start < end
+            pass
+
+    @abstractmethod
+    def UpdateCloud(self):
+        pass
+
+    @abstractmethod
+    def NewInterval(self):
+        pass
+
+    def __str__(self):
+        return f'{self.symbol}_{type(self).__name__.lower()}_{self.start}_{self.end}'
+
+    def prefix(self):
+        k = (type(self).__name__).lower()
+        return f'{self.symbol}_{k}'
+
+    @abstractmethod
+    def collectData(self): pass
+
+    def getlastestData(self):
+        pass
+
+    def cloud_Data(self):
+        self.cloud.downloadData(self.ValidFiles(),self.ValidFiles())
+        x = pd.read_csv(self.ValidFiles(), index_col='date')
+        duplicates = x.index.duplicated()
+        keep = duplicates == False
+        clean_data = x.loc[keep,:]
+        self.start = datetimeToDate(clean_data.index[-1])
+        self.end = datetimeToDate(clean_data.index[0])
+        clean_data.to_csv(self.fileFormat())
+
+class Weekly(MultiDay):
+    def __init__(self, symbol, start=None, end=None):
+        super().__init__(symbol, start=start, end=end)
+
+    def collectData(self,outputsize='compact') -> None:
+        ts = TimeSeries(key= key, output_format='pandas')
+        data , metadata = ts.get_weekly(self.symbol)
+        data = pd.DataFrame(data)
+        self.start = datetimeToDate(data.index[-1])
+        self.end = datetimeToDate(data.index[0])
+        self.properplace()
+        self.saveData(data)
+
+    def getlastestData(self):
+        ts = TimeSeries(key= key, output_format='pandas')
+        data , metadata = ts.get_weekly(self.symbol)
+        data = pd.DataFrame(data)
+        return data
+    
+    def NewInterval(self):
+        return isFriday() and marketclosed()
+
+    def UpdateCloud(self):
+        if self.NewInterval:
+            self.cloud_Data()
+
+class Monthly(MultiDay):
+    def __init__(self, symbol, start=None, end=None):
+        super().__init__(symbol, start=start, end=end)
+
+    def collectData(self,outputsize='compact') -> None:
+        ts = TimeSeries(key= key, output_format='pandas')
+        #optimaize in the future
+        data , metadata = ts.get_monthly(self.symbol)
+        data = pd.DataFrame(data)
+        self.start = datetimeToDate(data.index[-1])
+        self.end = datetimeToDate(data.index[0])
+        d = dateEndofMonth(datetimeToDate(data.index[0]))
+        self.properplace()
+        if  d:
+            self.saveData(data)
+            return None
+        new_data = data[1:]
+        self.end = datetimeToDate(new_data.index[0])
+        self.saveData(new_data)
+
+    def getlastestData(self):
+        ts = TimeSeries(key= key, output_format='pandas')
+        data , metadata = ts.get_monthly(self.symbol)
+        data = pd.DataFrame(data)
+        return data
+
+    def NewInterval(self):
+        return endofMonth() and marketclosed()
+
+    def UpdateCloud(self):
+        if self.NewInterval():
+            self.cloud_Data()
+
+class Daily(MultiDay):
+    def __init__(self, symbol, start=None, end=None):
+        super().__init__(symbol, start=start, end=end)
+
+    def collectData(self,outputsize='compact')-> None:
+        ts = TimeSeries(key= key, output_format='pandas')
+        #optimaize in the future
+        data , metadata = ts.get_daily(self.symbol,outputsize='full')
+        data = pd.DataFrame(data)
+        self.start = datetimeToDate(data.index[-1])
+        self.end = datetimeToDate(data.index[0])
+        self.properplace()
+        self.saveData(data)
+
+    def getlastestData(self):
+        ts = TimeSeries(key= key, output_format='pandas')
+        data , metadata = ts.get_daily(self.symbol,outputsize='full')
+        data = pd.DataFrame(data)
+        return data
+
+    def NewInterval(self):
+        return isWeekday() and marketclosed()
+
+    def UpdateCloud(self):
+        if self.NewInterval():
+            self.cloud_Data()
+
+
+
+### Helper functions
+def datetimeToDate(x:datetime):
+        return date( *[int(i) for i in x.__str__().split(' ')[0].split('-')])
+
+strToDate =lambda x : date(*[int(i) for i in str(x).split('-')])
+
+def isWeekday():
+    today = date.today().weekday()
+    return 7 > date.today().weekday() <= 4
+
+def markethours():
+    return time(9,30) < datetime.now().time() < time(16,3)
+
+def marketclosed():
+    return datetime.now().time() > time(16,3)
+
+def isFriday():
+    return date.today().weekday() == 4
+
+
+def endofMonth():
+    m = date.today().month
+    y = date.today().year
+    day,m_date = monthrange(y,m)
+    # print(day)
+    if 7 > day <= 4:
+        return date.today() == date(y,m,m_date)
+    else:
+        x = date(y,m,m_date)
+        while not 7 > x.weekday() <=4:
+            x - timedelta(days=1)
+            return date.today() == x
+
+def dateEndofMonth(date_data: date):
+    m = date_data.month
+    y = date_data.year
+    day,m_date = monthrange(y,m)
+    if 7 > day <= 4:
+        return date_data == date(y,m,m_date)
+    else:
+        x = date(y,m,m_date)
+        while not 7 > x.weekday() <=4:
+            x - timedelta(days=1)
+            return date_data == x
