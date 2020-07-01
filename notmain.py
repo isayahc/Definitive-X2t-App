cf8e9a4bdef15c999963f60f1191c0f72c3589ef
isayahc <isayahculbertson@gmail.com>
2020-07-01 07:36:45 -0400

    adding test for worker dyno

--- notmain.py
+++ notmain.py
@@ -7,4 +7,4 @@ def test():
 if __name__ == '__main__':
     sched = BlockingScheduler()
     x = datetime.now()
-    sched.add_job(test, 'cron', day_of_week='mon-fri', hour= 20,minute=4 )
+    sched.add_job(test, 'cron', day_of_week='mon-fri', hour= x.hour,minute=x.minute+1 )
