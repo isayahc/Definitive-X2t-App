12a021efc3851572d1fa52030d06a1b1c35a9ccf
Isayah Culbertson <isayahCulbertson@gmail.com>
2022-03-05 12:56:05 -0500

    Update tw.py

--- app/tw.py
+++ app/tw.py
@@ -1,11 +1,11 @@
 from twilio.rest import Client
 from config import APIs
-
+number = ''
 client = Client(APIs().getTwilioCreditals())
 message = client.messages.create(
                               body='Hi there! I cost money, bro',
                               from_='+1 205 304 5988',
-                              to='+13474320300'
+                              to=number
                           )
 
 print(message.sid)
